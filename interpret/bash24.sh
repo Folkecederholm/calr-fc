4df9a221424a96b2313b07795cfe2324f36c9b9b
Folke Cederholm <folkecederholm@gmail.com>
2025-12-30 18:18:15 +0100

    New system

--- interpret/bash24.sh
+++ interpret/bash24.sh
@@ -1,9 +1,3 @@
 #!/usr/bin/env bash
 
-sed '1s/^system/scheme-system/' $1 |
-sed '2s/^name/scheme-name/' |
-sed '3s/^author/scheme-author/' |
-sed '5d' |
-sed 's/^  //' |
-sed 's/^base\([0-9][0-9A-F]\)/base\1-hex/' |
-sed 's/#//'
+exit 0
