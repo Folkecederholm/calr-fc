2b86821dd2de7410fc61d1ceced4163def349e3c
Folke Cederholm <folkecederholm@gmail.com>
2025-12-30 10:46:38 +0100

    Update bash24.sh

--- interpret/bash24.sh
+++ interpret/bash24.sh
@@ -5,5 +5,5 @@ sed '2s/^name/scheme-name/' |
 sed '3s/^author/scheme-author/' |
 sed '5d' |
 sed 's/^  //' |
-sed 's/^base\([0-9][0-9]\)/base\1-hex/' |
+sed 's/^base\([0-9][0-9A-F]\)/base\1-hex/' |
 aws 's/#//'
