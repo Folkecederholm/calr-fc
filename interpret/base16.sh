b19b458d9830dab6ed30f6bee1ff5057ec9e4c0a
Folke Cederholm <folke@Rontgenium.local>
2025-12-30 10:34:39 +0100

    Add interpret files for base16 and base24.

---
+++ interpret/base16.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+sed '1s/^system/scheme-system/' $1 |
+sed '2s/^name/scheme-name/' |
+sed '3s/^author/scheme-author/' |
+sed '5d' |
+sed 's/^  //' |
+sed 's/^base\([0-9][0-9]\)/base\1-hex/' |
+aws 's/#//'
